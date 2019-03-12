b52e05b6583bec4ab8a7815f98ae4c3025889200
Juan Salazar <juanedsa@gmail.com>
2019-03-11 22:27:23 -0500

    Refactor ng Class

--- src/app/components/gem/gem.component.ts
+++ src/app/components/gem/gem.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { GemClass } from './gem-class';
+import { STONES } from './gem.constants';
 
 @Component({
   selector: 'app-gem',
@@ -7,4 +9,15 @@ import { Component, Input } from '@angular/core';
 })
 export class GemComponent {
   @Input() type: number;
+
+  public getClass(): GemClass {
+    return {
+      'gem--space': this.type === STONES.SPACE,
+      'gem--mind': this.type === STONES.MIND,
+      'gem--reality': this.type === STONES.REALITY,
+      'gem--time': this.type === STONES.TIME,
+      'gem--power': this.type === STONES.POWER,
+      'gem--soul': this.type === STONES.SOUL
+    }
+  }
 }
